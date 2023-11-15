738216f9d8a9d774d641e57ed25f0dc6a49841eb
shaik gulber <138758123+skgulber@users.noreply.github.com>
2023-11-15 18:28:49 +0530

    Update transpoce.c

--- transpoce.c
+++ transpoce.c
@@ -1,3 +1,8 @@
+input:1 2 3      output: 1  4  7
+      4 5 6              2  5  8
+      7 8 9              3  6  9
+
+
 #include<stdio.h>
 int main()
 {
@@ -7,7 +12,8 @@ for(i=0;i<2;i++)
 {
 for(j=0;j<3;j++)
 {
-scanf("%d",$a[i][j]);
+scanf("%d",&a[i][j]);
+
 }
 }
 printf("the transpose of a matrix is :\n");
