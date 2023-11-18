aa721a99b9bf2c36df86251f0a806de0653fe09c
shaik gulber <138758123+skgulber@users.noreply.github.com>
2023-11-18 14:28:32 +0530

    Update printarray.c

--- printarray.c
+++ printarray.c
@@ -1,5 +1,17 @@
+enter the array 1
+1
+2
+3
+4
+5
+6
+44
+8
+9
+10
+the elements array are:1	2	3	4	5	6	44	8	9	10	
 #include<stdio.h>
-inr main()
+int main()
 {
   int i,a[10];
 printf("enter the array");
@@ -7,7 +19,7 @@ for(i=0;i<10;i++)
 {
 scanf("%d",&a[i]);
 }
-printf("the elements array are::);
+printf("the elements array are:");
   for(i=0;i<10;i++)
   printf("%d\t",a[i]);
 }
