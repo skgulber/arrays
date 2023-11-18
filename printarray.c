68a2b74e956729b36676b500c32fe3d3cf96ceb1
shaik gulber <138758123+skgulber@users.noreply.github.com>
2023-11-18 14:03:33 +0530

    Create printarray.c

---
+++ printarray.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+inr main()
+{
+  int i,a[10];
+printf("enter the array");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("the elements array are::);
+  for(i=0;i<10;i++)
+  printf("%d\t",a[i]);
+}
