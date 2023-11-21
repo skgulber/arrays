fdf7791c28cd2ff0e1df9782755127f9b2a15878
shaik gulber <138758123+skgulber@users.noreply.github.com>
2023-11-21 20:05:32 +0530

    Update second largest.c

--- second largest.c
+++ second largest.c
@@ -13,8 +13,9 @@ max2=7
 #include<stdio.h>
 int main()
 {
-  printf("enter array elements:");
+ 
   int a[5],max1=0,max2=0;
+   printf("enter array elements:");
 for(int i=0;i<5;i++)
 {
 scanf("%d",&a[i]);
@@ -30,7 +31,7 @@ else if(a[i]>max2 && a[i]<max1)
 {
 max2=a[i];
 }
+}
 printf("max1=%d\n",max1);
 printf("max2=%d\n",max2);
-return 0;
-}}
+}
